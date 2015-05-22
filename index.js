acec7563276d5ac6a67c7f0bdcd6c70f82b88cc8
Eryk Napierała <eryk.piast@gmail.com>
2015-05-22 10:19:51 +0000

    add cross-origin requests tester

--- index.js
+++ index.js
@@ -1,9 +1,42 @@
+var fs = require('fs');
 var express = require('express');
+
 
 express()
     .get(/.*/, function(req, res) {
-        res.sendFile('index.html', {
-            root: __dirname
-        });
+        if(req.path.match(/\.css$/)) {
+            var status = req.path.match(/^\/([0-9]{3})/);
+            if(status) {
+                status = parseInt(status[1], 10);
+            }
+
+            if(!status) {
+                status = 500;
+            }
+
+            res.status(status);
+
+            fs.readFile(__dirname + req.path, { encoding: 'UTF-8' }, function(err, data) {
+                if(err) {
+                    res.send('Resource is not avaiable');
+                } else {
+                    res
+                        .type('text/css')
+                        .send(data);
+                }
+            });
+        } else if(req.path.match(/\.json$/)) {
+            var allowedDomain = req.query.restrictAccess || '*';
+
+            res
+                .set('Access-Control-Allow-Origin', allowedDomain)
+                .type('application/json')
+                .status(200)
+                .send('{ "data": "example data" }');
+        } else {
+            res.sendFile('index.html', {
+                root: __dirname
+            });
+        }
     })
     .listen(process.env.PORT);
